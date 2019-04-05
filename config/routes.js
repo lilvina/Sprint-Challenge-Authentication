bad333161eb8a2ceffba8a1a51165a06543983d8
lilvina <davinaleo2007@yahoo.com>
2019-04-05 10:54:56 -0700

    made some changes

--- config/routes.js
+++ config/routes.js
@@ -24,23 +24,22 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
-  let { username, password } = req.body
-  db('users').where({ username })
+  const { username, password } = req.body
+  db('users')
+    .where({ username })
     .first()
     .then(user => {
       if(user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
 
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}`,
           token,
         })
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' })
+        res.status(400).json({ message: 'Invalid credentials' })
       }
-    }).catch(err => {
-      res.status(500).json(err)
-    })
+    }).catch(err => res.status(500).json(err))
 }
 
 function getJokes(req, res) {
